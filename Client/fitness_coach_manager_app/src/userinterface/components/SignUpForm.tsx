3b147813cbed9da49cbe565e0a42777ba14c6ba6
Yannick <girofare@hotmail.com>
2023-09-21 18:23:08 +0200

    signup form created

--- Client/fitness_coach_manager_app/src/userinterface/components/SignUpForm.tsx
+++ Client/fitness_coach_manager_app/src/userinterface/components/SignUpForm.tsx
@@ -1,11 +1,49 @@
 "use client"
+
+import React, { ChangeEvent, FormEvent, useState } from 'react'
 import {Profile, SignUpFormProps} from '../../domain/entities/auth.types'
+
 const SignUpForm: React.FC<SignUpFormProps> = ({ onSignUp }) => {
+const [userDatas, setUserDatas] = useState<Profile>({
+    username: "",
+    email: "",
+    password_hash: "",
+    id: 0,
+    created_at: new Date(),
+})
+
+const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setUserDatas((prev) => ({...prev, [name]: value}))
+
+}
+const handleClick = async (e: FormEvent) => {
+    e.preventDefault();
+    onSignUp(userDatas.username, userDatas.password_hash, userDatas.email);
+  }
 
 
   return (
     <div className='wrapper' >
         <h1>Sign Up </h1>
+        <form className='forms'>
+  <label>
+   <h2> Username </h2> 
+    <input onChange={handleChange} className='inputform' type="text" name="username" value={userDatas.username} />
+  </label>
+
+  <label>
+     <h2> Email </h2> 
+    <input onChange={handleChange} className='inputform' type="text" name="email" value={userDatas.email}/>
+  </label>
+
+  <label>
+      <h2> password </h2> 
+    <input onChange={handleChange} className='inputform' type="text" name="password_hash" value={userDatas.password_hash} />
+  </label>
+
+</form>
+<button onClick={handleClick} >Inscription</button> 
         
     </div>
   )
